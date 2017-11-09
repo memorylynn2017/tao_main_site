dc68d849a52e6a99ae7490c564e4e5db5d733074
sky <jxsdifens@163.com>
2017-11-09 19:23:01 +0800

    express

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',
+  				city: "London",
+                street: "Baker Street",
+                number: "221B" });
+});
+
+module.exports = router;
