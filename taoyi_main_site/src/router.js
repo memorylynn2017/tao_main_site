db2fe23154ec5b33a51f908de3ed637949e745d7
memorylynn2017 <a95916127128@163.com>
2017-11-13 21:40:30 +0800

    commit work-r4

--- taoyi_main_site/src/router.js
+++ taoyi_main_site/src/router.js
@@ -1,6 +1,33 @@
 var express = require('express') ;
 var router = express.Router();
 
-//router.get("" , require('./controller/'));
+router.get("" , require('./controller/'));
 
 module.exports = router;
+
+// var express = require("express");
+// var router = express.Router();
+
+//测试用
+
+//router.post('/testing',require('./controller/testing.js'));
+
+// router.get("/role/get_all", require("./controllers/role/search.js"));
+
+// 这里依赖服务中心来调试debug...
+// router.get("/debug", (req, res) => {
+//   引入了base
+//   var base = require("base");
+//   var data = base.inner_status();
+//   res.send(data);
+// });
+
+// module.exports = router;
+
+
+
+
+
+
+
+
