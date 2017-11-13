a02cc3b8b1efd2108b2f597df9d0db446db38a5c
memorylynn2017 <a95916127128@163.com>
2017-11-13 21:45:11 +0800

    commit work-r5

--- taoyi_main_site/src/router.js
+++ taoyi_main_site/src/router.js
@@ -1,22 +1,27 @@
-var express = require('express') ;
-var router = express.Router();
 
-router.get("" , require('./controller/'));
+// 由于网站的路由请求get、post等比较多也可以建一个router文件夹来依类分配。。
 
-module.exports = router;
+// 第一段
+
+// var express = require('express') ;
+// var router = express.Router();
+
+// router.get("" , require('./controller/'));
+
+// module.exports = router;
 
 // var express = require("express");
 // var router = express.Router();
 
-//测试用
+// 第二段 测试用
 
 //router.post('/testing',require('./controller/testing.js'));
 
 // router.get("/role/get_all", require("./controllers/role/search.js"));
 
-// 这里依赖服务中心来调试debug...
-// router.get("/debug", (req, res) => {
-//   引入了base
+//   这里依赖服务中心来调试debug...
+//   router.get("/debug", (req, res) => {
+//   引入了base外部库或其它依赖
 //   var base = require("base");
 //   var data = base.inner_status();
 //   res.send(data);
