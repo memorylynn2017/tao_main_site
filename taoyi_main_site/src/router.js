1340de935c19968886da6f9115b812963cf25414
sky <jxsdifens@163.com>
2017-11-15 17:28:03 +0800

    加每日新款这几个页面

--- taoyi_main_site/src/router.js
+++ taoyi_main_site/src/router.js
@@ -26,6 +26,24 @@ router.get('/register', user_controller.register);
 //跳转到登录页面
 router.get('/login', user_controller.login);
 
+
+router.get('/shop/dailyNew', function(req, res, next) {
+    res.render('shop/dailyNew');
+});
+router.get('/shop/seller', function(req, res, next) {
+    res.render('shop/seller');
+});
+router.get('/shop/orderRemit', function(req, res, next) {
+    res.render('shop/orderRemit');
+});
+router.get('/shop/service', function(req, res, next) {
+    res.render('shop/service');
+});
+router.get('/shop/clearStore', function(req, res, next) {
+    res.render('shop/clearStore');
+});
+
+
 //跳转到搜索页面
 //router.get('/shop/product/search', user_controller.login);
 
