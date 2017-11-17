8dc902e1a8b44e6ef45787eb8d1d77dd68c4365e
memorylynn2017 <a95916127128@163.com>
2017-11-17 12:05:19 +0800

    不是一个人的项目，CSS及JS注意命名

--- taoyi_main_site/static/assets/js/common.js
+++ taoyi_main_site/static/assets/js/common.js
@@ -4,16 +4,16 @@
 * @Last Modified by:   86295
 * @Last Modified time: 2017-11-14 14:47:39
 */
-$(function() {
-  useTemplate($("#market-class-left"),data,$(".left-content ul"));
-  useTemplate($("#market-class-right"),data2,$(".mouthlist"));
-  function useTemplate(templateId,data,htmlBox) {
-    var t = templateId.html();
-    var f = Handlebars.compile(t);
-    var h = f(data);
-    htmlBox.html(h);
-    // console.log(h)
-    // console.log(t)
-    console.log($("#market-class-left").html())
-  }
-})
+// $(function() {
+//   useTemplate($("#market-class-left"),data,$(".left-content ul"));
+//   useTemplate($("#market-class-right"),data2,$(".mouthlist"));
+//   function useTemplate(templateId,data,htmlBox) {
+//     var t = templateId.html();
+//     var f = Handlebars.compile(t);
+//     var h = f(data);
+//     htmlBox.html(h);
+//     // console.log(h)
+//     // console.log(t)
+//     console.log($("#market-class-left").html())
+//   }
+// })
